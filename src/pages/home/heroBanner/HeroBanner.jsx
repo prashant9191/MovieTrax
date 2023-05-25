4b096ea6f915e3191a8c2c56aa39d7b1e9807f69
prashant9191 <sonupv000@gmail.com>
2023-05-25 17:35:32 +0530

    adding lazyloadimage component

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
-import "./style.scss"
-const HeroBanner = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import "./style.scss";
+import { useSelector } from "react-redux";
 
-export default HeroBanner
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)].backdrop_path;
+    setBackground(bg);
+  }, [data]);
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <div className="heroBanner">
+      <div className="backdrop-img">
+        <img src="" alt="" />
+      </div>
+      <div className="wrapper">
+        <div className="heroBannnerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explorer now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show..."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
