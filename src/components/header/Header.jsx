129ebea35ecf3fd24f9d0f15fdfa0b6fa201857a
prashant9191 <sonupv000@gmail.com>
2023-05-26 09:46:20 +0530

    adding header with mobile nav bar as well

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
-import "./style.scss";
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
 
-export default Header
+import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movieTrax-logo.svg";
+
+const Header = () => {
+    const [show, setShow] = useState("top");
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [query, setQuery] = useState("");
+    const [showSearch, setShowSearch] = useState("");
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    return (
+        <header className="header">
+          <ContentWrapper>
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            <ul className="menuItems">
+              <li className="menuItem">Movies</li>
+              <li className="menuItem">Tv Shows</li>
+              <li className="menuItem"><HiOutlineSearch/></li>
+            </ul>
+            <div className="mobileMenuItems">
+              <HiOutlineSearch/>
+              {mobileMenu ? <VscChromeClose/> :<SlMenu/>}
+              
+            </div>
+          </ContentWrapper>
+        </header>
+    );
+};
+
+export default Header;
