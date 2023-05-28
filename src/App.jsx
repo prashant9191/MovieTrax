14fec03e2cf56fde979831bf845df8c6b775c3c9
prashant9191 <sonupv000@gmail.com>
2023-05-28 13:50:50 +0530

    adding moviecard , spinner, video popup and switch tab

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,25 @@ function App() {
       dispatch(getApiConfiguration(url));
     });
   };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+        promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    // console.log(data);
+    data.map(({ genres }) => {
+        return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+};
+
   return (
     <BrowserRouter>
     <Header/>
