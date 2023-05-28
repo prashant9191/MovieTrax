2cf0585e940708820d72b9e7197e6977fc31d49a
prashant9191 <sonupv000@gmail.com>
2023-05-28 14:19:22 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,73 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
-import { useSelector, useDispatch } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
 
-// components
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
-import SearchReasult from "./pages/searchResult/SearchReasult";
+import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 
-
 function App() {
-  const dispatch = useDispatch();
-  const { url } = useSelector((state) => state.home);
-  useEffect(() => {
-    fetchApiConfig();
-  }, []);
-  const fetchApiConfig = () => {
-    fetchDataFromApi("/configuration").then((res) => {
-      console.log(res);
-      const url={
-        backdrop:res.images.secure_base_url + "original",
-        poster:res.images.secure_base_url + "original",
-        profile:res.images.secure_base_url + "original"
-      }
-      dispatch(getApiConfiguration(url));
-    });
-  };
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
 
-  const genresCall = async () => {
-    let promises = [];
-    let endPoints = ["tv", "movie"];
-    let allGenres = {};
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
 
-    endPoints.forEach((url) => {
-        promises.push(fetchDataFromApi(`/genre/${url}/list`));
-    });
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
+            console.log(res);
 
-    const data = await Promise.all(promises);
-    // console.log(data);
-    data.map(({ genres }) => {
-        return genres.map((item) => (allGenres[item.id] = item));
-    });
+            const url = {
+                backdrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
 
-    dispatch(getGenres(allGenres));
-};
+            dispatch(getApiConfiguration(url));
+        });
+    };
 
-  return (
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:mediaType/:id" element={<Details/>} />
-        <Route path="/search/:query" element={<SearchReasult />} />
-        <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-  );
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
+
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
+
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
+
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
